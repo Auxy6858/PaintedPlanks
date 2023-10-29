1e9207df842e3b056dd25c9cd4f33a0fe44aa248
Auxy6858 <71662994+Auxy6858@users.noreply.github.com>
2023-10-29 13:26:50 +0000

    First commit

---
+++ src/main/java/com/whirlpool/paintedplanks/block/FlammableModButtonBlock.java
@@ -0,0 +1,27 @@
+package com.whirlpool.paintedplanks.block;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.block.ButtonBlock;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.properties.BlockSetType;
+
+public class FlammableModButtonBlock extends ButtonBlock {
+    public FlammableModButtonBlock(Properties pProperties, BlockSetType pType, int pTicksToStayPressed, boolean pArrowsCanPress) {
+        super(pProperties, pType, pTicksToStayPressed, pArrowsCanPress);
+    }
+    @Override
+    public boolean isFlammable(BlockState state, BlockGetter level, BlockPos pos, Direction direction) {
+        return true;
+    }
+    @Override
+    public int getFlammability(BlockState state, BlockGetter level, BlockPos pos, Direction direction) {
+        return 10;
+    }
+
+    @Override
+    public int getFireSpreadSpeed(BlockState state, BlockGetter level, BlockPos pos, Direction direction) {
+        return 5;
+    }
+}
---
+++ src/main/java/com/whirlpool/paintedplanks/block/FlammableModPressurePlateBlock.java
@@ -0,0 +1,28 @@
+package com.whirlpool.paintedplanks.block;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.block.PressurePlateBlock;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.properties.BlockSetType;
+
+public class FlammableModPressurePlateBlock extends PressurePlateBlock {
+    public FlammableModPressurePlateBlock(Sensitivity pSensitivity, Properties pProperties, BlockSetType pType) {
+        super(pSensitivity, pProperties, pType);
+    }
+
+    @Override
+    public boolean isFlammable(BlockState state, BlockGetter level, BlockPos pos, Direction direction) {
+        return true;
+    }
+    @Override
+    public int getFlammability(BlockState state, BlockGetter level, BlockPos pos, Direction direction) {
+        return 10;
+    }
+
+    @Override
+    public int getFireSpreadSpeed(BlockState state, BlockGetter level, BlockPos pos, Direction direction) {
+        return 5;
+    }
+}
